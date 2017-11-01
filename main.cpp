b24f13b620f300ffdcb33082b7482a6e8df52c54
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-31 18:47:14 -0700

    Provide starter for int main only

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //DO NOT CHANGE WITHIN THIS AREA...
+  int red, blue, green;
+  cout<<"Enter Red, Green, and Blue values: ";
+  cin>>red>>green>>blue;
+  //...END OF "DO NOT CHANGE" AREA
+
+
+
+
+
+  //DO NOT CHANGE WITHIN THIS AREA...
+  return 0;
+  //...END OF "DO NOT CHANGE" AREA
+}
